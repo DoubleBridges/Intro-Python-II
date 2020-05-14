40f0609ffd26c88088d3ae12654bd0421bd7f275
Dave Kane <davelkane@gmail.com>
2020-05-13 23:30:03 -0500

    Added lightsource item, removed directional arguments from room class definiton

--- src/item.py
+++ src/item.py
@@ -1,4 +1,18 @@
-class Item():
-    def __init__(self, name, description):
+class Item:
+    def __init__(self, name: str, description: str):
         self.name = name
         self.description = description
+
+    def __str__(self):
+        return f"{self.name}, {self.description}"
+
+
+class Lightsource(Item):
+    def __init__(self, name: str, description: str, time_remaining: int, on: bool):
+        super().__init__(name, description)
+
+        self.time_remaining = time_remaining
+        self.on = on
+
+    def __str__(self):
+        super().__str__()
--- src/player.py
+++ src/player.py
@@ -2,34 +2,35 @@
 # currently.
 
 
-class Player():
-    def __init__(self, name, curr_room):
+class Player:
+    def __init__(self, name: str, curr_room: object):
         self.name = name
         self.curr_room = curr_room
         self.inventory = []
 
     def __str__(self):
-        return f'{self.name}\'s location: {self.curr_room}'
+        return f"{self.name}'s location: {self.curr_room}"
 
     def move(self, direction):
-        if getattr(self.curr_room, f"{direction}") is not None:
+        if hasattr(self.curr_room, f"{direction}"):
             self.curr_room = getattr(self.curr_room, f"{direction}")
         else:
             print("------- You can't walk through walls, Junior")
 
     def get(self, item):
-        if getattr(self.curr_room.items[f'{item}']):
+        if hasattr(self.curr_room.items, f"{item}"):
             self.inventory.append(item)
             self.curr_room.items.remove(item)
-            print(f'You are now have the {item}')
+            print(f"You now have the {item}")
         else:
-            print(f'{item} is not located in this room')
+            print(f"{item} is not located in this room")
 
     def drop(self, item):
-        if item in self.items:
+        if item in self.inventory:
             self.curr_room.items.append(item)
             self.inventory.remove(item)
             print(
-                f'{item} dropped in {self.curr_room}, hope you can remember where you left it')
+                f"{item} dropped in {self.curr_room}, hope you can remember where you left it"
+            )
         else:
             print("You gotta have it to lose it")
